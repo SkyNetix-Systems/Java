5acc4e5acbe30205edc895455b3dee83ed88db36
Akhilesh Shende <akhilesh@skynetixsystems.com>
2025-04-30 18:16:23 +0530

    added kafka

---
+++ LinkedList/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+class Node {
+    *prev : Node
+    Data data;
+    *next
+
+}
+
+class Data{
+    String name;
+    ----
+}
+
+add(*location, Data)
